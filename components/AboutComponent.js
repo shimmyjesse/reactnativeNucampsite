951ac6f62e34b8fb851c5b0d7eddee5abf2b221b
Yess Yess <shimmyjesse@gmail.com>
2020-06-05 14:16:43 -0700

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import {  Text, ScrollView, FlatList,} from "react-native";
 import { Card, ListItem } from 'react-native-elements'
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission() {
 
     return (
-        <Card
-            title={'Our Mission'}
-            wrapperStyle={{margin: 10}}>
-            <Text
-                style={{margin: 10}}>
-                    {'We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.'}
+        <Card title={'Our Mission'}>
+            <Text style={{margin: 10}}>
+                    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. 
+                    We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness 
+                    trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a 
+                    platform for campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
     );
 }
 
 class About extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -37,7 +37,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             )
         };
@@ -46,10 +46,9 @@ class About extends Component {
             <ScrollView>
                 <Mission />
                 <Card
-                    title={'Community Partners'}
-                    wrapperStyle={{margin: 10}}>
+                    title="Community Partners">
                     <FlatList
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}>
                     </FlatList>
@@ -59,4 +58,9 @@ class About extends Component {
     }
 }
 
-    export default About;
+// data={this.props.partners.partners}
+// first 'partners' refers to the entire part of the state that handles the partners data including the 'IS_LOADING' and
+//   the err_mess properties, along with the PARTNERS' array.
+// second 'partners' refers to PARTNERS' data array
+
+    export default connect(mapStateToProps)(About);
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,10 +1,10 @@
-export const baseUrl = 'http://172.17.66.161:3001/';
+export const baseUrl = 'http://192.168.1.166:3001/';
 
 // if you're experiencing issues communicating with the server, perhaps due to disconnecting and reconnecting
 // to your current network, or you've connected to a different network, this may cause this url to change.
 // First, 
 // Check you computer's current IP Address.
 // i.e. type ipconfig in your bash terminal to obtain the Windows IP Configuration and
-// look for the "IPv4 Address. . . . ", which, currently is 172.17.66.161. // for example //
+// look for the "IPv4 Address. . . . ", which, currently is 192.168.1.166. // for example //
 // Second,
 // Make sure your current IP Address matches the IP Address in the 'const basUrl' listed above.
