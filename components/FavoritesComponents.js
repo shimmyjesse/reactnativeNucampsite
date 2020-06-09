a67ff1d8f6eca08c7f2abc3160528dc961f6f13e
Yess Yess <shimmyjesse@gmail.com>
2020-06-08 19:22:18 -0700

    Swipeout and Delete Favorites but Campsite DOES NOT move

--- components/FavoritesComponents.js
+++ components/FavoritesComponents.js
@@ -4,6 +4,8 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 // This function in order to pass the campsites' data as props from the Redux store into the Favorites Component.
 const mapStateToProps = state => {
@@ -11,6 +13,10 @@ const mapStateToProps = state => {
         campsites: state.campsites,
         favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    deleteFavorite: campsiteId => (deleteFavorite(campsiteId))
 };
 
 class Favorites extends Component {
@@ -21,14 +27,24 @@ class Favorites extends Component {
 
     render() {
         const { navigate } = this.props.navigation;
+// for some reason, when swiping, "Delete" button shows up, but the favorited campsite does NOT move like in video(?)
         const renderFavoriteItem = ({item}) => {
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
             return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{ source: {uri: baseUrl + item.image }}}
-                    onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
-                />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        leftAvatar={{ source: {uri: baseUrl + item.image }}}
+                        onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
+                    />
+                </Swipeout>
             );
         };
 
@@ -53,4 +69,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
